f6188a3a5f87f13e641a27c688130f68a42de287
564258080@qq.com <564258080@qq.com>
2018-12-16 18:00:37 +0800

    solutions

---
+++ Array/Q39_Combination Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        # 1 1 2 5 6 7 10 target 8
