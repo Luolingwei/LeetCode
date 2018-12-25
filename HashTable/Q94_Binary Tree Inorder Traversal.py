e327ab92712ac9620879059df92ce0c70c0f5787
Lingwei Luo <564258080@qq.com>
2018-12-25 18:25:02 +0800

    solutions

---
+++ HashTable/Q94_Binary Tree Inorder Traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        ans=[]
+        if not root:
+            return []
+        ans+=self.inorderTraversal(root.left)
+        ans+=[root.val]
+        ans+=self.inorderTraversal(root.right)
+        return ans
