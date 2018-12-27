0fa713d09f19cf1f682a5fe50844160644b99715
Lingwei Luo <564258080@qq.com>
2018-12-27 10:28:38 +0800

    solutions

--- Array/Q123_Best Time to Buy and Sell Stock III.py
+++ Array/Q123_Best Time to Buy and Sell Stock III.py
@@ -4,27 +4,29 @@ class Solution:
         :type prices: List[int]
         :rtype: int
         """
-        if not prices:
+        if len(prices)<=1:
             return 0
-        max_profit=0
-        min_price=prices[0]
-        for price in prices:
-            min_price=min(min_price,price)
-            max_profit=max(price-min_price,max_profit)
-        return max_profit
-
-    def maxProfit2(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        total=self.maxProfit(prices)
+        total=0
+        left_price = prices[0]
+        right_price = prices[-1]
+        left_profit = [0]*len(prices)
+        right_profit = [0]*len(prices)
         for i in range(1,len(prices)):
-            total=max(total,self.maxProfit(prices[:i])+self.maxProfit(prices[i:]))
+            # total=max(total,self.maxProfit2(prices[:i])+self.maxProfit2(prices[i-1:]))
+            left_price = min(left_price, prices[i])
+            left_profit[i] = max(prices[i] - left_price, left_profit[i-1])
+
+        for i in range(len(prices)-2,-1,-1):
+            right_price = max(right_price, prices[i])
+            right_profit[i] = max(right_price-prices[i], right_profit[i+1])
+
+        for i in range(len(prices)):
+            total=max(total,left_profit[i]+right_profit[i])
+
         return total
 
 a=Solution()
-print(a.maxProfit2([3,3,5,0,0,3,1,4]))
-print(a.maxProfit2([1,2,3,4,5]))
-print(a.maxProfit2([7,6,4,3,1]))
+print(a.maxProfit([3,3,5,0,0,3,1,4]))
+print(a.maxProfit([1,2,3,4,5]))
+print(a.maxProfit([7,6,4,3,1]))
 
