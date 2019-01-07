21c8b330781671d2dd4f99923bc608cdfd56b245
Lingwei Luo <564258080@qq.com>
2019-01-07 19:05:09 +0800

    solutions

---
+++ LinkedList/Q23_Merge k Sorted Lists.py
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        root = prehead = ListNode(0)
+        while l1 and l2:
+            if l1.val > l2.val:
+                prehead.next = l2
+                prehead = prehead.next
+                l2 = l2.next
+            else:
+                prehead.next = l1
+                prehead = prehead.next
+                l1 = l1.next
+        if not l1:
+            prehead.next = l2
+        if not l2:
+            prehead.next = l1
+        return root.next
+
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if not lists:
+            return None
+        if len(lists)==1:
+            return lists[0]
+        else:
+            first=lists[:len(lists)//2]
+            second=lists[len(lists)//2:]
+            return self.mergeTwoLists(self.mergeKLists(first),self.mergeKLists(second))
+
+
