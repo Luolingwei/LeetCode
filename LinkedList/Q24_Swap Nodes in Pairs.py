c3a88e83414e6e039b9c3492fa121061b25c22e5
Lingwei Luo <564258080@qq.com>
2019-01-08 13:44:10 +0800

    solutions

---
+++ LinkedList/Q24_Swap Nodes in Pairs.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head is None or head.next is None:
+            return head
+        root=head.next
+        node=head.next.next
+        head.next.next=head
+        head.next=self.swapPairs(node)
+        return root
