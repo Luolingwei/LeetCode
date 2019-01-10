ef1c222c649f85629728afc90baedd5e48e3f546
Lingwei Luo <564258080@qq.com>
2019-01-10 18:41:20 +0800

    solutions

---
+++ LinkedList/Q82_Remove Duplicates from Sorted List II.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prehead = ListNode(float('inf'))
+        prehead.next = head
+        last = prehead
+        node = head
+        while node and node.next:
+            node = node.next
+            while node and node.val == last.next.val:
+                node = node.next
+            if node==last.next.next:
+                last = last.next
+            else:
+                last.next = node
+        return prehead.next
+
+
+
+
+
