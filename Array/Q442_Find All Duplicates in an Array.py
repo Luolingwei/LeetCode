39bae622abc73e52c71b9e1a1aa9429eefb00228
Lingwei Luo <564258080@qq.com>
2019-01-18 11:45:09 +0800

    solutions

--- Array/Q442_Find All Duplicates in an Array.py
+++ Array/Q442_Find All Duplicates in an Array.py
@@ -15,7 +15,17 @@ class Solution:
         # return list(ans)
 
         #solution2
-        return [num for num, n in collections.Counter(nums).items() if n==2]
+        # return [num for num, n in collections.Counter(nums).items() if n==2]
+
+        #solution3
+        ans=[]
+        for num in nums:
+            index=abs(num)-1
+            if nums[index]<0:
+                ans.append(index+1)
+            else:
+                nums[index]=-nums[index]
+        return ans
 
 a=Solution()
 print(a.findDuplicates([4,3,2,7,8,2,3,1]))
