94d1634510dddbf5624b16fc6fd3798ebd272477
Lingwei Luo <564258080@qq.com>
2019-01-21 18:50:27 +0800

    solutions

--- Array/Q39_Combination Sum.py
+++ Array/Q39_Combination Sum.py
@@ -1,8 +1,21 @@
 class Solution:
+    def dfs(self,nums,target,start,res,path):
+        if target<0:
+            return
+        if target==0:
+            res.append(path)
+        for i in range(start,len(nums)):
+            self.dfs(nums,target-nums[i],i,res,path+[nums[i]])
     def combinationSum(self, candidates, target):
         """
         :type candidates: List[int]
         :type target: int
         :rtype: List[List[int]]
         """
-        # 1 1 2 5 6 7 10 target 8
+        res=[]
+        self.dfs(candidates,target,0,res,[])
+        return res
+
+a=Solution()
+print(a.combinationSum([2,3,6,7],7))
+print(a.combinationSum([2,3,5],8))
