394d77b570df431ab80fe6c869d41c19763331e0
Lingwei Luo <564258080@qq.com>
2019-01-24 19:29:34 +0800

    solutions

---
+++ LinkedList/Q430_Flatten a Multilevel Doubly Linked List.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+class Solution:
+    def flatten(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        stack=[]
+        p=head
+        while head:
+            if head.child:
+                if head.next:
+                    stack.append(head.next)
+                head.next=head.child
+                head.next.prev=head
+                head.child=None
+            if not head.next and stack:
+                head.next=stack.pop()
+                head.next.prev=head
+            head=head.next
+        return p
