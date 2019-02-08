00cd726085d157e34a4bdd6ad1da7dfd3ed8c3a2
564258080@qq.com <564258080@qq.com>
2019-02-08 22:34:40 +0800

    solutions

---
+++ String/Q32_Longest Valid Parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestValidParentheses(self, s):
+        stack, result = [(-1, ')')], 0
+        for i, paren in enumerate(s):
+            if paren == ')' and stack[-1][1] == '(':
+                stack.pop()
+                result = max(result, i - stack[-1][0])
+            else:
+                stack += (i, paren),
+        return result
+
+a=Solution()
+print(a.longestValidParentheses(")()((()))"))
+print(a.longestValidParentheses("(()"))
+print(a.longestValidParentheses(")()())"))
+print(a.longestValidParentheses(")))))"))
+print(a.longestValidParentheses("((()))()(())"))
