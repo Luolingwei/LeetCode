bfd5fbe60fc052a7a39c18c3873dff337f2f47da
564258080@qq.com <564258080@qq.com>
2019-02-23 17:24:08 +0800

    solutions

--- Array/Q78_Subsets.py
+++ Array/Q78_Subsets.py
@@ -1,13 +1,27 @@
 class Solution:
+    #solution1 Iteratively
+    # def subsets(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: List[List[int]]
+    #     """
+    #     ans=[[]]
+    #     for num in nums:
+    #         ans+=[item+[num] for item in ans]
+    #     return ans
+
+    #solution2 dfs
+    def dfs(self,nums,start,path,ans):
+        ans.append(path)
+        for i in range(start,len(nums)):
+            self.dfs(nums,i+1,path+[nums[i]],ans)
     def subsets(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        ans=[[]]
-        for number in nums:
-            for element in list(ans):
-                ans.append([number]+element)
+        ans=[]
+        self.dfs(nums,0,[],ans)
         return ans
 
 a=Solution()
