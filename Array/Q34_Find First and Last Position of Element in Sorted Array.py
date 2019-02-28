fb20edb13121d60a72da05c5e5fb8a61d146d9f9
Lingwei Luo <564258080@qq.com>
2019-02-28 17:36:32 +0800

    solutions

--- Array/Q34_Find First and Last Position of Element in Sorted Array.py
+++ Array/Q34_Find First and Last Position of Element in Sorted Array.py
@@ -1,45 +1,26 @@
 class Solution:
+    def getRange(self,nums,center):
+        left,right=center,center
+        while left>-1 and nums[left]==nums[center]: left-=1
+        while right<len(nums) and nums[right]==nums[center]: right+=1
+        return [left+1,right-1]
+
     def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        l=len(nums)
-        i=0
-        startpoint=None
-        endpoint= None
-
-        if nums==[]:
-            return [-1,-1]
-        if l==1:
-            if nums[0]==target:
-                return [0,0]
+        low,high=0,len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return self.getRange(nums,mid)
+            elif nums[mid]>target:
+                high=mid-1
             else:
-                return [-1,-1]
-
-        while i<l:
-            if startpoint==None:
-                if nums[i]==target:
-                    startpoint=i
-                else:
-                    i=i+1
-            else:
-                if endpoint == None:
-                    if nums[i]!=target:
-                        endpoint=i-1
-                    else:
-                        i=i+1
-
-            if startpoint!=None and endpoint!=None:
-                break
-
-        if startpoint == None and endpoint == None:
-            return [-1,-1]
-        elif startpoint!=None and endpoint== None:
-            return [startpoint,l-1]
-        else:
-            return [startpoint,endpoint]
+                low=mid+1
+        return [-1,-1]
 
 a=Solution()
 print(a.searchRange([1,4,6],4))
@@ -47,3 +28,5 @@ print(a.searchRange([1,4,4,4,4,4,4],4))
 print(a.searchRange([4,4,4,4,4,4,1,1],4))
 print(a.searchRange([4,5,5,5,5,6,7],5))
 print(a.searchRange([1,5,6,7,8,9,10],6))
+print(a.searchRange([4,5],6))
+print(a.searchRange([],6))
