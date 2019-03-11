6efffabde1f05e7e1edcd9d04adcb95987d7c35f
Lingwei Luo <564258080@qq.com>
2019-03-11 17:38:29 +0800

    solutions

--- Array/Q41_First Missing Positive.py
+++ Array/Q41_First Missing Positive.py
@@ -4,13 +4,17 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        if nums==[]:
-            return 1
-        l=len(nums)
-        i=1
-        while i in nums:
-            i+=1
-        return i
+        nums.sort()
+        ans=1
+        for num in nums:
+            if num<ans:
+                continue
+            elif num==ans:
+                ans+=1
+                continue
+            else:
+                break
+        return ans
 
 a=Solution()
 print(a.firstMissingPositive([1,2,0]))
@@ -19,5 +23,3 @@ print(a.firstMissingPositive([7,8,9,11,12]))
 print(a.firstMissingPositive([-2,1,2,4,5]))
 print(a.firstMissingPositive([-8,8,-2,2,2]))
 print(a.firstMissingPositive([-1,0,1,1,1,2,4]))
-
-
