07783d705ccb92b66e38167c844863f5e31032e2
llw <564258080@qq.com>
2019-03-20 10:45:26 +0800

    solutions

---
+++ Tree/Q102_Binary Tree Level Order Traversal.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # solution 1 recursive
+    # def helper(self,root,ans,level):
+    #     if root:
+    #         if len(ans)<level+1:
+    #             ans.append([])
+    #         ans[level].append(root.val)
+    #         self.helper(root.left,ans,level+1)
+    #         self.helper(root.right,ans,level+1)
+    # def levelOrder(self, root):
+    #     ans=[]
+    #     self.helper(root,ans,0)
+    #     return ans
+
+    # solution 2 iterative
+    def levelOrder(self, root):
+        ans,stack=[],[(root,0)]
+        while stack:
+            node,level=stack.pop()
+            if node:
+                if len(ans)<level+1:
+                    ans.append([])
+                ans[level].append(node.val)
+                stack.append((node.right,level+1))
+                stack.append((node.left,level+1))
+        return ans
