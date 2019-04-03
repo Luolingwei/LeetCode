90f71dbfcf316a9de4780c8e5978c648c47b26c3
llw <564258080@qq.com>
2019-04-03 17:27:08 +0800

    solutions

--- Array/Q74_Search a 2D Matrix.py
+++ Array/Q74_Search a 2D Matrix.py
@@ -1,14 +1,15 @@
 class Solution:
     def binary_search(self, list, target):
-        if list==[]:
-            return False
-        mid=len(list)//2
-        if list[mid]==target:
-            return True
-        elif list[mid]>target:
-            return self.binary_search(list[:mid],target)
-        else:
-            return self.binary_search(list[mid+1:],target)
+        l,r=0,len(list)-1
+        while l<=r:
+            mid=(l+r)//2
+            if list[mid]==target:
+                return True
+            elif list[mid]<target:
+                l=mid+1
+            else:
+                r=mid-1
+        return False
 
     def searchMatrix(self, matrix, target):
         """
@@ -16,17 +17,17 @@ class Solution:
         :type target: int
         :rtype: bool
         """
-        if matrix==[]:
-            return False
-        row=-1
-        n=len(matrix)
-        for i in range(n):
-            if matrix[i]==[]:
-                return False
-            if matrix[i][0]>target:
-                row=i-1
-                break
-        return self.binary_search(matrix[row],target)
+        if len(matrix)==0 or len(matrix[0])==0: return False
+        for i in range(len(matrix)):
+            if matrix[i][-1]>=target:
+                return self.binary_search(matrix[i],target)
+        return False
+
 
 a=Solution()
+print(a.searchMatrix([[-9,-7,-7,-5,-3],[-1,0,1,3,3],[5,7,9,11,12],[13,14,15,16,18],[19,21,22,22,22]],-4))
 print(a.searchMatrix([[1,3,5,7],[10, 11, 16, 20],[23, 30, 34, 50]],18))
+print(a.searchMatrix([[1,3,5,20]],18))
+print(a.searchMatrix([[]],1))
+print(a.searchMatrix([],1))
+print(a.searchMatrix([[1]],1))
