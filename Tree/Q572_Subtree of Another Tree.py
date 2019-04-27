d33ca1f52bab44a8324828cac9988b249cf41ef4
564258080@qq.com <564258080@qq.com>
2019-04-27 15:18:42 +0800

    solutions

--- Tree/Q572_Subtree of Another Tree.py
+++ Tree/Q572_Subtree of Another Tree.py
@@ -8,19 +8,13 @@
 
 class Solution:
     def sametree(self,tree1,tree2):
-        if not tree1 and not tree2:
-            return True
         if not tree1 or not tree2:
-            return False
+            return tree1==tree2
         if tree1.val!=tree2.val:
             return False
         return self.sametree(tree1.left,tree2.left) and self.sametree(tree1.right,tree2.right)
 
     def isSubtree(self, s, t):
-        if self.sametree(s,t):
-            return True
-        if s:
-            if self.isSubtree(s.left,t) or self.isSubtree(s.right,t):
-                return True
-            else:
-                return False
+        if not s:
+            return False
+        return self.sametree(s,t) or self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
