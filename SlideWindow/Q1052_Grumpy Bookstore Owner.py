2b3c528e25534891ffd6e09af2d35cc89c91d9ae
Luolingwei <564258080@qq.com>
2019-05-27 17:25:51 +0800

    solutions

--- SlideWindow/Q1052_Grumpy Bookstore Owner.py
+++ SlideWindow/Q1052_Grumpy Bookstore Owner.py
@@ -3,12 +3,11 @@
 
 class Solution:
     def maxSatisfied(self, customers, grumpy, X):
-        left,gap,maxGap=0,0,0
-        for i in range(len(grumpy)):
-            gap+=customers[i]*grumpy[i]
-            if i-left>=X:
-                gap,left=gap-customers[left]*grumpy[left],left+1
-            maxGap=max(maxGap,gap)
+        window=maxGap=sum([customers[i]*grumpy[i] for i in range(X)])
+        for i in range(X,len(grumpy)):
+            window+=customers[i]*grumpy[i]
+            window-=customers[i-X]*grumpy[i-X]
+            maxGap=max(maxGap,window)
         return sum([customers[i]*abs(grumpy[i]-1) for i in range(len(customers))])+maxGap
 
 a=Solution()
--- SlideWindow/Q643_Maximum Average Subarray I.py
+++ SlideWindow/Q643_Maximum Average Subarray I.py
@@ -5,14 +5,13 @@ class Solution:
         :type k: int
         :rtype: float
         """
-        biggest=total=sum(nums[:k])
-        start=0
+        window=biggest=sum(nums[:k])
         for i in range(k,len(nums)):
-            total-=nums[start]
-            total+=nums[i]
-            biggest=max(biggest,total)
-            start+=1
+            window+=nums[i]
+            window-=nums[i-k]
+            biggest=max(biggest,window)
         return biggest/k
 
 a=Solution()
 print(a.findMaxAverage([1,12,-5,-6,50,3],4))
+print(a.findMaxAverage([-1],1))
