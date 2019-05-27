851be318f750a8175f9e478935ab591600de4ab1
Luolingwei <564258080@qq.com>
2019-05-27 12:04:21 +0800

    solutions

---
+++ SlideWindow/Q1052_Grumpy Bookstore Owner.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxSatisfied(self, customers, grumpy, X):
+        left,gap,maxGap=0,0,0
+        for i in range(len(grumpy)):
+            gap+=customers[i]*grumpy[i]
+            if i-left>=X:
+                gap,left=gap-customers[left]*grumpy[left],left+1
+            maxGap=max(maxGap,gap)
+        return sum([customers[i]*abs(grumpy[i]-1) for i in range(len(customers))])+maxGap
+
+a=Solution()
+print(a.maxSatisfied([1,0,1,2,1,1,7,5],[0,1,0,1,0,1,0,1],3))
+print(a.maxSatisfied([1,5,1,2],[0,1,1,1],3))
