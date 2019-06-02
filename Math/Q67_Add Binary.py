ca186c7a72e649db4e4a0878a2bb71fe377db8a9
564258080@qq.com <564258080@qq.com>
2019-06-02 22:41:42 +0800

    solutions

--- Math/Q67_Add Binary.py
+++ Math/Q67_Add Binary.py
@@ -1,36 +1,13 @@
-class Solution(object):
+class Solution:
     def addBinary(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
-        ans=''
-        carry=0
-        a=a[::-1]
-        b=b[::-1]
-        for i in range(max(len(a),len(b))+1):
-            if i>len(a)-1 and i<=len(b)-1:
-                n=int(b[i])+carry
-            elif i>len(b)-1 and i<=len(a)-1:
-                n = int(a[i])+carry
-            elif i>len(a)-1 and i>len(b)-1:
-                if carry==1:
-                    n=carry
-                else:break
-            else:
-                n= int(a[i])+int(b[i])+carry
+        a,b=[int(i) for i in a],[int(j) for j in b]
+        ans,carry='',0
+        while a or b or carry:
+            carry+=(a or [0]).pop()+(b or [0]).pop()
+            ans=str(carry&1)+ans
+            carry>>=1
+        return ans
 
-            carry=0
-            if n<2:
-                ans+=str(n)
-            elif n==2:
-                ans+=str(0)
-                carry=1
-            else:
-                ans+=str(1)
-                carry=1
-        return ans[::-1]
 
 a=Solution()
 print(a.addBinary("11","1"))
