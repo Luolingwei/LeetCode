5ac4f7ec7082681d7eeaa63db7c4cc17fc43b1a9
Luolingwei <564258080@qq.com>
2019-07-19 11:22:00 +0800

    solutions

--- LinkedList/Q206_Reverse Linked List.py
+++ LinkedList/Q206_Reverse Linked List.py
@@ -5,14 +5,21 @@
 #         self.next = None
 
 class Solution:
+    # Solution 1 recursive 40 ms
+    # def reverseList(self, head):
+    #     if not head or not head.next:
+    #         return head
+    #     new_head=self.reverseList(head.next)
+    #     head.next.next=head
+    #     head.next=None
+    #     return new_head
+
+    # Solution 2 iterative
     def reverseList(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        if not head or not head.next:
-            return head
-        new_head=self.reverseList(head.next)
-        head.next.next=head
-        head.next=None
-        return new_head
+        pre=None
+        while head:
+            next_head=head.next
+            head.next=pre
+            pre=head
+            head=next_head
+        return pre
