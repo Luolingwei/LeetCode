da73f2f739588e64141661270762c3faa0c84b0f
Luolingwei <564258080@qq.com>
2019-07-22 17:24:54 +0800

    solutions

--- Tree/Q100_Same Tree.py
+++ Tree/Q100_Same Tree.py
@@ -6,10 +6,26 @@
 #         self.right = None
 
 class Solution:
+    # Solution 1 recursive 40 ms
+    # def isSameTree(self, p,q):
+    #     if p and q:
+    #         return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    #     elif not p and not q:
+    #         return True
+    #     else:
+    #         return False
+
+    # Solution 2 iterative 36 ms
     def isSameTree(self, p,q):
-        if p and q:
-            return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        elif not p and not q:
-            return True
-        else:
-            return False
+        stack=[(p,q)]
+        while stack:
+            l,r=stack.pop()
+            if l and r:
+                if l.val!=r.val: return False
+                stack.append((l.left,r.left))
+                stack.append((l.right,r.right))
+            elif not l and not r:
+                continue
+            else:
+                return False
+        return True
