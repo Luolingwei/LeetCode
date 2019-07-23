0199ae40ed6d6c7356bcc9a226eeea732303c010
Luolingwei <564258080@qq.com>
2019-07-23 18:36:18 +0800

    solutions

--- DynamicProgramming/Q174_Dungeon Game.py
+++ DynamicProgramming/Q174_Dungeon Game.py
@@ -1,17 +1,16 @@
 #  Input: [[-2,-3,3],[-5,-10,1],[10,30,-5]]
 # Output: 7
 
-# 思路: 倒着考虑，dp[i][j]表示i,j位置至少需要的H值，dp[i][j]=max(1,min(dp[i+1][j],dp[i][j+1]))
+# 思路: 倒着考虑，dp[i][j]表示i,j位置至少需要的H值，dp[i][j]=max(1,min(dp[i+1][j],dp[i][j+1])-board[i][j])
 
 class Solution:
     def calculateMinimumHP(self, board):
         m,n=len(board),len(board[0])
-        dp=[[0]*n+[float('inf')] for _ in range(m)]+[[float('inf')]*(n+1)]
-        dp[m-1][n-1]=max(1,1-board[-1][-1])
+        dp=[[float('inf')]*(n+1) for _ in range(m+1)]
+        dp[m-1][-1],dp[-1][n-1]=1,1
         for i in range(m)[::-1]:
             for j in range(n)[::-1]:
-                if i+j!=m+n-2:
-                    dp[i][j]=max(1,min(dp[i+1][j],dp[i][j+1])-board[i][j])
+                dp[i][j]=max(1,min(dp[i+1][j],dp[i][j+1])-board[i][j])
         return dp[0][0]
 
 a=Solution()
