9104fe1d2a2238d304736eb1d9850b21761c0ca9
564258080@qq.com <564258080@qq.com>
2019-09-19 13:37:43 -0400

    solutions

--- BinarySearch/Q153_Find Minimum in Rotated Sorted Array.py
+++ BinarySearch/Q153_Find Minimum in Rotated Sorted Array.py
@@ -1,7 +1,9 @@
 
-# 思路: 用nums[mid]和nums[r]的大小关系判断最小值所落在的区间.
+# 思路1: 用nums[mid]和nums[r]的大小关系判断最小值所落在的区间.
+# 思路2: divide and conquer, 分成左右两个数组，分别考虑是否递增(必有一个递增)
 
 class Solution:
+    # Solution 1 binary search
     def findMin(self, nums):
         l,r=0,len(nums)-1
         while l<r:
@@ -11,6 +13,14 @@ class Solution:
             else:
                 r=mid
         return nums[l]
+
+    # Solution 2 divide and conquer
+    # def findMin(self, nums):
+    #     if len(nums)<=2: return min(nums)
+    #     if nums[0]<nums[-1]: return nums[0]
+    #     mid=len(nums) // 2
+    #     return min(self.findMin(nums[:mid]),self.findMin(nums[mid:]))
+
 
 a=Solution()
 print(a.findMin([3,4,5,1,2]))
