55ca13bb8c0ffb07314b2d7c48fa34091b73c65a
564258080@qq.com <564258080@qq.com>
2019-09-21 18:44:38 -0400

    solutions

--- Math/Q50_Pow(x, n).py
+++ Math/Q50_Pow(x, n).py
@@ -1,23 +1,36 @@
-class Solution(object):
+# Input: 2.00000, 10
+# Output: 1024.00000
+
+# 思路: divide and conquer,
+
+class Solution:
+
+    # Solution 1 iterative
+    # def myPow(self, x, n):
+    #     sign=1 if n<0 else 0
+    #     ans,index,n=1,0,abs(n)
+    #     while index<n:
+    #         temp,carry=x,1
+    #         while n-index>=carry:
+    #             ans = ans * temp
+    #             index += carry
+    #             temp=temp*temp
+    #             carry=2*carry
+    #     if sign==1:
+    #         return 1/ans
+    #     else:
+    #         return ans
+
     def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        sign=1 if n<0 else 0
-        ans,index,n=1,0,abs(n)
-        while index<n:
-            temp,carry=x,1
-            while n-index>=carry:
-                ans = ans * temp
-                index += carry
-                temp=temp*temp
-                carry=2*carry
-        if sign==1:
-            return 1/ans
-        else:
-            return ans
+        def Pow(x,n):
+            if n==0:
+                return 1
+            temp=Pow(x,n//2)
+            if n%2==0:
+                return temp*temp
+            else:
+                return x*temp*temp
+        return Pow(x,n) if n>=0 else 1/Pow(x,-n)
 
 a=Solution()
 print(a.myPow(2,-2))
