00b80e4efe22e2c1330c1b496c6d1da6b56b9797
564258080@qq.com <564258080@qq.com>
2019-11-04 14:15:29 -0500

    solutions

--- Design/Q225_Implement Stack using Queues.py
+++ Design/Q225_Implement Stack using Queues.py
@@ -1,43 +1,33 @@
+
+# 往非空的一个queue中push，pop时把非空队列往空队列中转，剩一个元素，pop出此元素，后进先出
+
+from collections import deque
 class MyStack:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.queue1,self.queue2=[],[]
+        self.q1, self.q2 = deque(), deque()
 
     def push(self, x: int) -> None:
-        """
-        Push element x onto stack.
-        """
-        self.queue1.insert(0,x)
-    def pop(self) -> int:
-        """
-        Removes the element on top of the stack and returns that element.
-        """
+        if self.q1:
+            self.q1.append(x)
+        else:
+            self.q2.append(x)
 
-        while len(self.queue1)>1:
-            self.queue2.insert(0,self.queue1.pop())
-        num=self.queue1.pop()
-        while self.queue2:
-            self.queue1.insert(0,self.queue2.pop())
-        return num
+    def pop(self) -> int:
+        if not self.q1:
+            while len(self.q2) > 1:
+                self.q1.append(self.q2.popleft())
+            return self.q2.popleft()
+        else:
+            while len(self.q1) > 1:
+                self.q2.append(self.q1.popleft())
+            return self.q1.popleft()
 
     def top(self) -> int:
-        """
-        Get the top element.
-        """
-        return self.queue1[0]
+        if self.q1:
+            return self.q1[-1]
+        else:
+            return self.q2[-1]
 
     def empty(self) -> bool:
-        """
-        Returns whether the stack is empty.
-        """
-        return not self.queue1
-
-# Your MyStack object will be instantiated and called as such:
-# obj = MyStack()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.empty()
+        return not self.q1 and not self.q2
