b5eb5a3ff64f95d9d6218c9c2fab283b2baa688c
564258080@qq.com <564258080@qq.com>
2020-01-19 16:41:22 +0800

    solutions

--- Greedy/Q1326_Minimum Number of Taps to Open to Water a Garden.py
+++ Greedy/Q1326_Minimum Number of Taps to Open to Water a Garden.py
@@ -1,7 +1,10 @@
 
-# 思路: 将所有喷泉转为interval存储，左边界取max(0,l)，按left排序，left相同的，取最大的right
+# 思路1: 将所有喷泉转为interval存储，左边界取max(0,l)，按left排序，left相同的，取最大的right
 # 从第0个开始，对于当前end范围内的left，拿最大的right进行范围扩展，(所有范围都要cover，所以下一个的left一定在当前end内)
 # 结束条件为end到达末尾，在循环中如果某轮end无法再被扩展(curmax<=end)，直接break
+
+# 思路2: dp, dp[i]表示cover i之前范围需要开的喷泉个数，对于每个喷泉(i,r)，其cover的范围是i-r到i+r
+# 对于i-r到i+r的j, dp[j]=min(dp[j],dp[i-r]+1)表示开启当前喷泉，并更新此范围内的所有dp[i]
 
 class Solution:
     def minTaps(self, n, ranges):
@@ -19,6 +22,15 @@ class Solution:
             end=curmax
             ans+=1
         return ans if end>=n else -1
+
+
+    # def minTaps(self, n: int, ranges: List[int]) -> int:
+    #     dp = [0] + [float('inf')] * n
+    #     for i, r in enumerate(ranges):
+    #         for j in range(max(0, i - r), min(n, i + r) + 1):
+    #             dp[j] = min(dp[j], dp[max(0, i - r)] + 1)
+    #     return dp[n] if dp[n] != float("inf") else -1
+
 
 a=Solution()
 print(a.minTaps(5,[3,4,1,1,0,0]))
