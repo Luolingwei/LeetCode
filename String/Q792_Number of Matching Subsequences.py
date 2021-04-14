10f078d0d25fd95f71fe0cc32e129bc0ae492b84
LingweiLuo <564258080@qq.com>
2021-04-14 15:48:53 -0400

    solutions

--- String/Q792_Number of Matching Subsequences.py
+++ String/Q792_Number of Matching Subsequences.py
@@ -5,24 +5,63 @@
 # Output: 3
 # Explanation: There are three words in words that are a subsequence of S: "a", "acd", "ace".
 
-# 思路: 用iter进行subsequence的check，用set()记录已经check过的字符串，避免重复check.
+# 思路1: 对于每个w, 和s进行check是否是subsequence, O(N*LengthS), 由于本题S很长, time-consuming
+# 思路2: 用iter, 仍然需要每次check S
+# 思路3: 以S出发对各个word进行check, 每次匹配开头的一个字母的wordList, 直到wordList中的word所有字符被匹配(剩余长度变为0)
+
+from collections import Counter, defaultdict
 
 class Solution:
-    def numMatchingSubseq(self, S, words):
-        ans,right,false=0,set(),set()
-        for word in words:
-            if word in right:
-                ans+=1
-                continue
-            elif word in false:
-                continue
-            it=iter(S)
-            if all(char in it for char in word):
-                ans+=1
-                right.add(word)
-            else:
-                false.add(word)
-        return ans
+    def numMatchingSubseq1(self, s, words):
+        def check(w, s):
+            i, j, m, n = 0, 0, len(w), len(s)
+            while i < m and j < n:
+                while j < n and s[j] != w[i]:
+                    j += 1
+                if j < n:
+                    i += 1
+                    j += 1
+            return i == m
+
+        count, res = Counter(words), 0
+
+        for w in count.keys():
+            if check(w, s):
+                res += count[w]
+        return res
+
+    def numMatchingSubseq2(self, s, words):
+        count = Counter(words)
+        res = 0
+        for w in count.keys():
+            it_S = iter(s)
+            if all(c in it_S for c in w):
+                res += count[w]
+        return res
+
+    def numMatchingSubseq3(self, s, words):
+        to_match, res = defaultdict(list), 0
+
+        for w in words:
+            to_match[w[0]].append(w)
+
+        for c in s:
+            if c in to_match:
+                cur_match = to_match.pop(c)
+                for w in cur_match:
+                    if len(w) == 1:
+                        res += 1
+                    else:
+                        to_match[w[1]].append(w[1:])
+        return res
+
 
 a=Solution()
-print(a.numMatchingSubseq("abcde",["a", "bb", "acd", "ace"]))
+print(a.numMatchingSubseq1("abcde",["a", "bb", "acd", "ace"]))
+print(a.numMatchingSubseq1("dsahjpjauf",["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]))
+
+print(a.numMatchingSubseq2("abcde",["a", "bb", "acd", "ace"]))
+print(a.numMatchingSubseq2("dsahjpjauf",["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]))
+
+print(a.numMatchingSubseq3("abcde",["a", "bb", "acd", "ace"]))
+print(a.numMatchingSubseq3("dsahjpjauf",["ahjpjau","ja","ahbwzgqnuk","tnmlanowax"]))
--- String/Q843_Guess the Word.py
+++ String/Q843_Guess the Word.py
@@ -1,6 +1,13 @@
 
-# 思路: 建立索引的集合，随机pop一个word，计算curscore，剩下的words与当前word的score等于curscore才可能是secret.
-# 首先对wordlist进行随机化，用首字母排序
+# 思路1: 每次随机选择一个word, 得到score, 留下list中和当前guess match结果为score的word
+
+# 思路2: 因为最有可能的情况是一个字符都不match, 概率为 (25/26)^6 = 80%, 所以最有可能的情况是首先guess到match score=0,
+# 可以从candidates中选择和其他word的 match score = 0 数量最小的word, 这样candidates 集合减小最快
+# O(N^2)
+
+# 思路3: 根据char frequency的总和 选择family 最大的那个word, 这样一旦guess到match score=0, 所有和这个word有重叠的candidate都会被剔除掉
+# 理论上guess所有位置freq最大char组成的word, 可以一次剔除最多的candidate, 但是本题只能在wordlist中guess
+# O(N)
 
 # """
 # This is Master's API interface.
@@ -9,18 +16,48 @@
 # class Master:
 #     def guess(self, word: str) -> int:
 
+import random
+from collections import Counter
 class Solution:
-    def findSecretWord(self, wordlist, master):
-        def myguess(word, match):
-            ans = 0
-            for i in range(6):
-                if word[i] == match[i]:
-                    ans += 1
-            return ans
 
-        wordlist.sort(key=lambda x: x[0])
-        wordsets = set(range(len(wordlist)))
-        while wordsets:
-            curi = wordsets.pop()
-            score = master.guess(wordlist[curi])
-            wordsets = {i for i in wordsets if myguess(wordlist[i], wordlist[curi]) == score}
+    def findSecretWord1(self, wordlist, master):
+        def match(x, y):
+            return sum(x[i] == y[i] for i in range(len(x)))
+
+        for _ in range(10):
+            word = random.choice(wordlist)
+            score = master.guess(word)
+            wordlist = [w for w in wordlist if match(w, word) == score]
+
+
+    def findSecretWord2(self, wordlist, master):
+        def match(x, y):
+            return sum(x[i] == y[i] for i in range(len(x)))
+
+        def select(wordlist):
+            count = Counter()
+            for i in range(len(wordlist)):
+                for j in range(len(wordlist)):
+                    if j != i and match(wordlist[i], wordlist[j]) == 0:
+                        count[i] += 1
+            return min(range(len(wordlist)), key=lambda x: count[x])
+
+        for _ in range(10):
+            word = wordlist[select(wordlist)]
+            score = master.guess(word)
+            wordlist = [w for w in wordlist if match(w, word) == score]
+
+
+    def findSecretWord3(self, wordlist, master) -> None:
+        def match(x, y):
+            return sum(x[i] == y[i] for i in range(len(x)))
+
+        def select(wordlist):
+            freqs = [Counter(w[i] for w in wordlist) for i in range(6)]
+            word = max(wordlist, key=lambda x: sum(freqs[i][c] for i, c in enumerate(x)))
+            return word
+
+        for _ in range(10):
+            word = select(wordlist)
+            score = master.guess(word)
+            wordlist = [w for w in wordlist if match(w, word) == score]
